a43a1c0765e10cbdca0a68cc68993c4c529efbb5
foxyara44 <nikolayfokin444@gmail.com>
2024-10-10 23:15:25 +0200

    123

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -10,4 +10,4 @@ def hello():
     return f"Hello {name}! {message}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
